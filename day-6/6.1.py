a6b6309b20ee98214bf45747e53ff2dbde845ef1
Widzew12 <szmnglowacz@gmail.com>
2025-12-06 13:20:27 +0100

    Add day 6 part 1

---
+++ day-6/6.1.py
@@ -0,0 +1,44 @@
+with open("input.txt", "r") as input_file:
+    input_data = [line[:-1] for line in input_file.readlines()]
+
+len_line = len(input_data[0])
+
+div_locations = []
+for i in range(len_line):
+    is_div = True
+    for line in input_data:
+        if line[i] != " ":
+            is_div = False
+            break
+    
+    if is_div:
+        div_locations.append(i)
+
+len_loc = len(div_locations)
+operations = []
+
+for i in range(len_loc + 1):
+    start = 0 if i == 0 else div_locations[i - 1]
+    end = len_line if i == len_loc else div_locations[i]
+    new_op = []
+    for line in input_data:
+        new_op.append(line[start:end].strip())
+    
+    operations.append(new_op)
+
+grand_total = 0
+
+for operation in operations:
+    nums, op = operation[:-1], operation[-1]
+    if op == "+":
+        res = 0
+        for num in nums:
+            res += int(num)
+    else:
+        res = 1
+        for num in nums:
+            res *= int(num)
+    
+    grand_total += res
+
+print(grand_total)
