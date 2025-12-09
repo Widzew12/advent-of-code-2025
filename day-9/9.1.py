c9a9da90e463c7dd6632e800152e50b6062ede2a
Widzew12 <szmnglowacz@gmail.com>
2025-12-09 13:28:48 +0100

    Add day 9 part 1

---
+++ day-9/9.1.py
@@ -0,0 +1,12 @@
+with open("input.txt", "r") as input_file:
+    red_positions = [(int(x), int(y)) for x, y in [line.strip().split(",") for line in input_file.readlines()]]
+
+max_area = 0
+for i in range(len(red_positions)):
+    x1, y1 = red_positions[i]
+    for j in range(i + 1, len(red_positions)):
+        x2, y2 = red_positions[j]
+        area = abs((x1 - x2 + 1) * (y1 - y2 + 1))
+        max_area = max(area, max_area)
+
+print(max_area)
